fa4bb9ddc9fa96eb50c8f152933494a0c54c10b1
Eddy Gonzalez <gon19005@byui.edu>
2022-03-25 04:16:52 -0600

    calculator app. Performance the basic operations in a calculator without decimals

---
+++ Calculator-app.swift
@@ -0,0 +1,8 @@
+//
+//  Calculator-app.swift
+//  Calculator
+//
+//  Created by Eddy Gonzalez on 3/25/22.
+//
+
+import Foundation
---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Eddy Gonzalez on 3/18/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
